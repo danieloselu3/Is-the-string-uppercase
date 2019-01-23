82d74c714972ec177ab45da2f1bc9955b0366596
danieloselu3 <doselu3@gmail.com>
2019-01-23 14:51:25 +0300

    added a travis file

--- is_the_string_uppercase.py
+++ is_the_string_uppercase.py
@@ -2,10 +2,10 @@
 import string
 
 #define your method
-def is_uppercase(str):
+def is_uppercase(strng):
     small_caps_alphabet = string.ascii_lowercase    #create a variable that holds all the lowercase alphabets
     container = []                                  #create an empty container to hold any identified lowercase alphabets
-    for char in str:                                #loop over each character in the str
+    for char in strng:                                #loop over each character in the str
         if char in small_caps_alphabet:             #check if there is any lowercase character in str
             container.append(char)                  # if any lowercase character is found, append it to container
     return True if len(container) == 0 else False   #return True if the container is empty(no lowercase alphabets were found)
